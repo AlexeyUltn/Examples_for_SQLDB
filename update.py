e933cd1f3890760ac864159d2195960aa9108464
AlexeyUltn <108830159+AlexeyUltn@users.noreply.github.com>
2023-02-26 14:21:56 +0300

    Update update.py

--- update.py
+++ update.py
@@ -14,7 +14,7 @@ con_to_table_from = psycopg2.connect(database='', user='', password='', host='',
 cur_to_take_from = con_to_table_from.cursor()
 
 def max_id(cur_updatable):
-  query = """select max("id") from updatable_table"""
+  query = """select max("id") from updatable_table;"""
   cur_updatable.execute(query)
   result = cur_updatable.fetchone()[0]
   return result
@@ -22,9 +22,9 @@ def max_id(cur_updatable):
 counter = 0
 for i in range (0, max_id(cur_updatable)):
   counter += 1
-  query_updatable = """select date from updatable_table where id = {}""".format(i)
+  query_updatable = """select date from updatable_table where id = {};""".format(i)
   result_updatable = cur_updatable.fetchone()[0]
-  query_for_table_from = """select IPO, company_name where date_ipo = {}""".format(result_updatable)
+  query_for_table_from = """select IPO, company_name where date_ipo = {};""".format(result_updatable)
   cur_to_take_from.execute(query_for_table_from)
   result_from = cur_to_take_from.fetchone
   try:
@@ -33,7 +33,7 @@ for i in range (0, max_id(cur_updatable)):
   except Exception as D:
     print(D)
   try:
-    query_to_update = """update updatable_table set IPO = '{}', company_name = '{}' where id = '{}' """.format(IPO, company_name, i)
+    query_to_update = """update updatable_table set IPO = '{}', company_name = '{}' where id = '{}';""".format(IPO, company_name, i)
     cur_updatable_new.execute(query_to_update)
   except Exception as A:
   print(A)
